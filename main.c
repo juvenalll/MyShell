83db4f714288d93e4739541b47b5d4e756f6bd27
Juvenal <jarellanobooking@gmail.com>
2019-10-05 18:00:27 -0400

    progress

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <std.io>
+
+void interactive_mode(char *commands){
+    printf("myshell>");
+    scanf("%s", commands);
+}
+
+int main(int argc, char[]  *argv){
+    while(1){
+        if (argc == 0){
+            char future_input[256];
+            interactive_mode(future_input);
+            }
+    
+    return 0;
+    }
+    
+}
